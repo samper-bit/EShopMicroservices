866eefa31176270c8226218bad4257783065d47d
User <yarik.sawchuck3102004@gmail.com>
2025-01-21 20:53:24 +0200

    Custom Exception Handling

--- src/Services/Catalog/Catalog.API/Exceptions/ProductNotFoundException.cs
+++ src/Services/Catalog/Catalog.API/Exceptions/ProductNotFoundException.cs
@@ -1,3 +1,10 @@
-﻿namespace Catalog.API.Exceptions;
+﻿using BuildingBlocks.Exceptions;
 
-public class ProductNotFoundException() : Exception("Product not found!");
+namespace Catalog.API.Exceptions;
+
+public class ProductNotFoundException : NotFoundException
+{
+    public ProductNotFoundException(Guid Id) : base("Product", Id)
+    {
+    }
+}
